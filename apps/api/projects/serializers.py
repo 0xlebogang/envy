2e544e82f26de6a407677f675f14a68b1be51fba
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-22 17:30:07 +0200

    configure projects app

---
+++ apps/api/projects/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Project
+from typing import Dict
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
+        read_only_fields = ("created_at", "updated_at")
+
+    def validate(self, attrs: Dict) -> Dict:
+        """
+        Validate the project attributes. Checks if the name is provided.
+        """
+        if not attrs.get("name"):
+            raise serializers.ValidationError("Project name is required.")
+        return attrs
