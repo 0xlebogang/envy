aa55f7124f952022b225dddcf3e076760ada536e
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-22 18:22:56 +0200

    add variables serializer class

---
+++ apps/api/variables/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Variable
+from typing import Dict
+
+
+class VariableSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Variable model.
+    """
+
+    class Meta:
+        model = Variable
+        fields = "__all__"
+        read_only_fields = ("created_at", "updated_at", "user")
+        extra_kwargs = {
+            "project": {"read_onnly": True},
+            "user": {"read_only": True},
+        }
+
+    def validate(self, attrs: Dict) -> Dict:
+        """
+        Custom validation to ensure that the variable name is unique within the project.
+        """
+        required_fields = {"name", "value"}
+
+        # Check if required fields are present in the attributes
+        if not required_fields.issubset(set(attrs.keys())):
+            raise serializers.ValidationError("Name and value are required fields.")
+
+        return attrs
