8a8c67387f5eed73f1471bcfa464ee1947b0b6dd
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-22 14:29:35 +0200

    add users models

---
+++ apps/api/users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from .managers import UserManager
+from django.db import models
+
+
+class User(AbstractUser, PermissionsMixin):
+    """
+    Custom user model that extends AbstractUser and PermissionsMixin.
+    This model uses email as the unique identifier instead of username.
+    """
+
+    username = None
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
