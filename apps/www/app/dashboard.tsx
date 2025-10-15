6cf413db0241b90453af813febeeb3df0184e0fa
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-10-15 15:15:57 +0200

    feat: update dashboard page card blocks (#54)

--- apps/www/app/dashboard.tsx
+++ apps/www/app/dashboard.tsx
@@ -1,13 +1,28 @@
+import { Card, CardContent, CardHeader } from "@repo/ui/components/card";
 import { useSession } from "next-auth/react";
 
 export default function Dashboard() {
 	const session = useSession();
 
 	return (
-		<main className="min-h-[calc(100vh-88px)] flex items-center justify-center">
-			<h1 className="text-4xl font-bold">
-				Signed in as {session.data?.user?.email}
-			</h1>
+		<main className="min-h-[calc(100vh-88px)] grid grid-cols-2 gap-6">
+			<Card>
+				<CardHeader>
+					<h2 className="text-2xl font-bold">
+						Welcome, {session.data?.user?.name}!
+					</h2>
+				</CardHeader>
+			</Card>
+			<Card>
+				<CardHeader>
+					<h2 className="text-2xl font-bold">Your Projects</h2>
+				</CardHeader>
+				<CardContent>
+					<p className="text-gray-500">
+						You have no projects yet. Start by creating a new project!
+					</p>
+				</CardContent>
+			</Card>
 		</main>
 	);
 }
