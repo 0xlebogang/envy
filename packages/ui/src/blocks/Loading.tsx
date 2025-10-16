ecb2e5917ad9360be65cf6cfacb7c780b7efe65a
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-10-16 15:52:59 +0200

    chore: disable cookie lint rule

--- packages/ui/src/blocks/Loading.tsx
+++ packages/ui/src/blocks/Loading.tsx
@@ -3,7 +3,12 @@ import { cn } from "@repo/ui/lib/utils";
 
 export default function Loading({ className }: { className?: string }) {
 	return (
-		<div className={cn("w-full flex flex-col items-center justify-center", className)}>
+		<div
+			className={cn(
+				"w-full flex flex-col items-center justify-center",
+				className,
+			)}
+		>
 			<Spinner className="h-8 w-8" />
 		</div>
 	);
