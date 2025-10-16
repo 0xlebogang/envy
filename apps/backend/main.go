172cc4aa49b38647ca91268fdf1816435b012431
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-10-16 15:30:20 +0200

    feat: add server entrypoint

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"context"
+	"time"
 
+	"github.com/0xlebogang/envy/internal/config"
+)
+
+func main() {
+	conf := config.LoadEnv()
+
+	server := config.NewServer(conf)
+	server.RegisterRoutes()
+	server.Start()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer server.Shutdown(ctx)
+	defer cancel()
 }
