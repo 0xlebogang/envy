b254c2f40fd8863470a9dab97a27a79b7e4f8b6a
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-15 14:17:17 +0200

    chore: add react testing library deps

--- packages/vitest-config/src/configs/ui-config.ts
+++ packages/vitest-config/src/configs/ui-config.ts
@@ -1,9 +1,12 @@
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineProject, mergeConfig } from "vitest/config";
 import { baseConfig } from "./base-config.js";
 
 export const uiConfig = mergeConfig(
 	baseConfig,
 	defineProject({
+		plugins: [react(), tsconfigPaths()],
 		test: {
 			environment: "jsdom",
 		},
