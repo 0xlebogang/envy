c4c9cb7d4f94486028e126769760b22b365942b1
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-15 14:00:29 +0200

    chore(refractor): add footer config to improve testability

---
+++ apps/web/src/components/Footer/config.tsx
@@ -0,0 +1,27 @@
+import Logo from "../Logo";
+
+interface FooterConfig {
+	logo: React.ReactElement;
+	links: {
+		label: string;
+		href: string;
+		external?: boolean;
+	}[];
+}
+
+export const footerConfig: FooterConfig = {
+	logo: <Logo />,
+	links: [
+		{ label: "Documentation", href: "/docs" },
+		{
+			label: "GitHub",
+			href: "https://github.com/envy/",
+			external: true,
+		},
+		{ label: "Community", href: "/community" },
+		{
+			label: "License",
+			href: "/licenses",
+		},
+	],
+};
