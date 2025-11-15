e4cd3735a748c7bdf30fdb6619e7a372841d2e3f
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-15 20:43:29 +0200

    update root vitest config

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,3 +1,8 @@
 import { uiConfig } from "@repo/vitest-config";
+import { mergeConfig } from "vitest/config";
 
-export default uiConfig;
+export default mergeConfig(uiConfig, {
+	test: {
+		setupFiles: ["./vitest.setup.ts"],
+	},
+});
