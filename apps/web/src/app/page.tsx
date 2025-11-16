bbf7b4bea57362a94f56966831170a2fa9e42bb5
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-16 15:32:00 +0200

    chore: update section component naming convention

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,6 @@
 import CodePreview from "@/components/CodePreview";
-import CTA from "@/components/sections/CTA";
-import Hero from "@/components/sections/Hero";
+import CTA from "@/components/sections/call-to-action";
+import Hero from "@/components/sections/hero";
 
 export default function Index() {
 	return (
