9f40edc33b23c080ee25f2bbf0380a3d042c5ac0
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-16 11:54:24 +0200

    chore: move footer component

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { DM_Sans } from "next/font/google";
 
 import "@repo/shadcn/styles/globals.css";
-import Footer from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import { Providers } from "@/components/Providers";
+import Footer from "@/components/sections/Footer";
 
 const fontSans = DM_Sans({
 	subsets: ["latin"],
