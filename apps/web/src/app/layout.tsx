df38afb8901d6d935f1bbe8277fe077a6414be9f
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-17 18:40:28 +0200

    add themed selection coloring

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<body className={`${fontSans.variable} font-sans antialiased`}>
+			<body
+				className={`${fontSans.variable} font-sans antialiased selection:bg-primary selection:text-foreground`}
+			>
 				<Providers>
 					<Navbar />
 					{children}
