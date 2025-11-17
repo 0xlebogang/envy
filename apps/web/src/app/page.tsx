15db2cc883d0b52646572a7faccff157a89d8853
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-17 16:58:25 +0200

    feat: add sidebar component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,32 @@
+import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
 import CodePreview from "@/components/CodePreview";
 import CTA from "@/components/sections/call-to-action";
 import Hero from "@/components/sections/hero";
 
+let isAuthenticated: boolean;
+
+if (process.env.NODE_ENV !== "production") {
+	// Replace with actual authentication check once protected routes are implemented
+	isAuthenticated = true;
+}
+
 export default function Index() {
+	if (!isAuthenticated) {
+		return (
+			<>
+				<Hero />
+				<CodePreview />
+				<CTA />
+			</>
+		);
+	}
+
 	return (
-		<>
-			<Hero />
-			<CodePreview />
-			<CTA />
-		</>
+		<AdminPanelLayout>
+			<ContentLayout title="TEST">
+				<p>Welcome to the admin panel!</p>
+			</ContentLayout>
+		</AdminPanelLayout>
 	);
 }
