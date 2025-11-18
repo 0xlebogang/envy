732523e2310df0e6e8a5d19b2ecf666942fcfe62
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-18 15:24:14 +0200

    add global test mock imports

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -1,1 +1,22 @@
 import "@testing-library/jest-dom/vitest";
+import { vi } from "vitest";
+
+// Mock components & modules
+// Next.js
+vi.mock("next/font/google");
+vi.mock("next/navigation");
+vi.mock("next/link");
+
+// Shadcn UI components
+vi.mock("@repo/shadcn/components/navigation-menu");
+vi.mock("@repo/shadcn/components/sheet");
+vi.mock("@repo/shadcn/components/button");
+vi.mock("@repo/shadcn/components/dropdown-menu");
+vi.mock("@repo/shadcn/components/avatar");
+
+// Zustand stores
+vi.mock("@/stores/auth-store");
+vi.mock("@/stores/home-renderer-store");
+
+// Lucide React icons
+vi.mock("lucide-react");
