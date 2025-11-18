97e83dcaa047639498870023ee27f823beb46466
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-18 14:46:35 +0200

    chore: add utils mocked  implementation

--- apps/web/__mocks__/@repo/shadcn/lib/utils.ts
+++ apps/web/__mocks__/@repo/shadcn/lib/utils.ts
@@ -1,3 +1,6 @@
-export function cn(...inputs: string[]) {
+import { vi } from "vitest";
+
+// let cn return any type for easier mocking in tests
+export const cn: any = vi.fn().mockImplementation((...inputs: string[]) => {
 	return inputs.filter(Boolean).join(" ");
-}
+});
