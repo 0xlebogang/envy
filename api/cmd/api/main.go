dc5c9c69cdbc452e29736f0b469ccff385de4ca1
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-30 10:43:54 +0200

    feat: add dev service compose setup

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/0xlebogang/sekrets/internal/config"
 	"github.com/0xlebogang/sekrets/internal/server"
 )
 
 func main() {
-	s := server.New("8080")
+	cfg := config.LoadEnv()
+
+	s := server.New(cfg.Port)
 	if err := s.Start(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
