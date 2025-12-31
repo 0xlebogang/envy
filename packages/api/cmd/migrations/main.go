954cfe4468ad5662bc59e86ed1e0235032d3b081
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-31 19:12:19 +0200

    feat: add db migration

--- packages/api/cmd/migrations/main.go
+++ packages/api/cmd/migrations/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"github.com/0xlebogang/sekrets/internal/config"
+	"github.com/0xlebogang/sekrets/internal/database"
+	"github.com/0xlebogang/sekrets/internal/models"
+	"gorm.io/driver/postgres"
+)
+
 func main() {
-	// Run database migrations
+	connectionString := config.Load().DatabaseUrl
+	db, err := database.Connect(postgres.Open(connectionString))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		panic(err)
+	}
 }
