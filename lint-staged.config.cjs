10b9c27874e52c2ffa8f23460e1a79e26962fa6a
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-01-28 02:28:13 +0200

    chore: update precommit linting

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,14 +1,5 @@
 const path = require("path");
 
-const buildGoLintCommand = (files) => {
-	const commands = files.map((file) => {
-		const dir = path.dirname(file);
-		return `golangci-lint run --fix --tests=false ${path.join(dir, path.basename(file))}`;
-	});
-	return commands;
-};
-
 module.exports = {
 	"*.{js,cjs,mjs,ts,mts,tsx,json,css}": "biome check --fix",
-	"*.go": buildGoLintCommand,
 };
