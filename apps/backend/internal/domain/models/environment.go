28b1c4935f452bd1a5c173089a22c59ebcfa6807
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-03-26 20:36:19 +0200

    feat(backend): add secret model

--- apps/backend/internal/domain/models/environment.go
+++ apps/backend/internal/domain/models/environment.go
@@ -5,4 +5,5 @@ type Environment struct {
 	ProjectID   string    `json:"project_id"`
 	Name        string    `json:"environment" gorm:"type:varchar(50);not null" binding:"min=2,max=50"`
 	Description *string   `json:"description" gorm:"type:text;default:null" binding:"omitempty,min=2,max=250"`
+	Secrets     []Secret
 }
